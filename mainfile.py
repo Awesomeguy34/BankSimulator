23106028be554b27678ef08de0fb913d6d229792
Awesomeguy34 <78974129+Awesomeguy34@users.noreply.github.com>
2024-07-19 21:53:48 -0400

    creation of code for basic window
    setup main file for application. created code for basic window and some test functionality

--- mainfile.py
+++ mainfile.py
@@ -1,1 +1,19 @@
+#imports tkinter module and allows for window to be created
+from tkinter import *
+root = Tk()
+#title of root window and creation of window
+root.title("welcome to BankSimulator")
+#set width and height
+root.geometry('800x500')
 
+#all widgets go down here
+
+#creates label to welcome user
+label_1 = Label(root, text = "welcome to the bank")
+label_1.grid()
+
+#button widget
+main_button = Button(root, text = "test", fg="red",)
+main_button.grid(column=1, row=0)
+#executes tkinter
+root.mainloop()
